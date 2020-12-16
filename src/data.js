cb3d88e05e424cc5a440ee1d11906157610ce007
akhaniShaik <shaikakhani84@gmail.com>
2020-12-16 14:06:24 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: "virat kohli",
+    age: 31,
+    image:
+      "https://lh3.googleusercontent.com/proxy/q36JDfhO1ZbCgtBnKsN3w2RWvGx3iKYqHb19DRrkERk1KGbADrAIfw015PfEB07yZf_I2F0dpM5ZHg2D495aKg0MaTPwFsf2Cg",
+  },
+  {
+    id: 2,
+    name: "Hester Hogan",
+    age: 32,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
+  },
+  {
+    id: 3,
+    name: "Larry Little",
+    age: 36,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+  },
+  {
+    id: 4,
+    name: "Sean Walsh",
+    age: 34,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 5,
+    name: "Lola Gardner",
+    age: 29,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+];
